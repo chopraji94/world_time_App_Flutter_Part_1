94ace86b1fcb2b911bdc55a7a14b64f41edb7429
chopraji94 <41725998+chopraji94@users.noreply.github.com>
2020-10-26 14:56:16 +0530

    Using async code for API's

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,6 +6,29 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  void getData() async {
+
+    //simulate a netrok request from a username
+    String username = await Future.delayed(Duration(seconds: 3),() {
+      return 'Pranav';
+    });
+
+    //simulate network request to get bio ofusername
+    String bio = await Future.delayed(Duration(seconds: 2),() {
+      return 'BIO' ;
+    });
+
+    print('$username-$bio');
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
